3bbdab4663e2bf79367ef7ba7b9d31aba4187b65
Roeland Cerfonteijn <NL12143@hotmail.com>
2025-09-18 12:13:01 +0200

    Create ListKeys-HKLU.ps1

---
+++ scripts/ListKeys-HKLU.ps1
@@ -0,0 +1,16 @@
+
+Get-ChildItem -Path "HKCU:\Software\Policies\" -Recurse
+
+Get-ChildItem -Path "HKLU:\Software\Policies\" -Recurse | ForEach-Object {
+    $keyPath = $_.PSPath
+    $props = Get-ItemProperty -Path $_.PSPath
+    foreach ($name in $props.PSObject.Properties.Name) {
+        if ($name -ne "PSPath" -and $name -ne "PSParentPath" -and $name -ne "PSChildName" -and $name -ne "PSDrive" -and $name -ne "PSProvider") {
+            [PSCustomObject]@{
+                KeyPath   = $keyPath
+                Property  = $name
+                Value     = $props.$name
+            }
+        }
+    }
+} | Format-Table -AutoSize
